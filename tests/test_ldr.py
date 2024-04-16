a233efc171b415a3b932f18fab23cc74afed77b6
Kylian Jourdaine <kylianjourdaine@gmail.com>
2024-04-16 11:54:36 +0200

    Modification code de la LDR

--- tests/test_ldr.py
+++ tests/test_ldr.py
@@ -2,10 +2,24 @@
 from machine import Pin, I2C, ADC
 import time
  
-ldr = ADC(26)
- 
+# Configuration du broche analogique pour la lecture de la photorésistance
+pin_photoreceptor = ADC(26)  # Utilisez le bon numéro de broche selon votre configuration
+
+# Fonction pour convertir la valeur lue en pourcentage de lumière
+def convert_to_light_percentage(sensor_value, max_value=65535):
+    light_percentage = (sensor_value / max_value) * 100
+    return light_percentage
+
+# Boucle principale
 while True:
-    val = ldr.read_u16()
-    print(val)
-    print((val*100)/65536)
-    time.sleep(2)
+    # Lecture de la valeur de la photorésistance
+    sensor_value = pin_photoreceptor.read_u16()
+    
+    # Conversion en pourcentage de lumière
+    light_percentage = convert_to_light_percentage(sensor_value)
+    
+    # Affichage du pourcentage de lumière
+    print("Pourcentage de lumière : {:.2f}%".format(light_percentage))
+    
+    # Attente avant la prochaine lecture
+    time.sleep(1)  # Attendez 1 seconde entre chaque lecture
