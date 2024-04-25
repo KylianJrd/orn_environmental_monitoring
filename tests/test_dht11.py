2f3ddcb0e7cb9aedb37e281dca44bae0561531b1
Kylian Jourdaine <kylianjourdaine@gmail.com>
2024-04-25 10:48:36 +0200

    modification of script DHT11

--- tests/test_dht11.py
+++ tests/test_dht11.py
@@ -1,18 +1,19 @@
-from machine import Pin, I2C, ADC
+from machine import Pin
 from time import sleep
-import dht
+import dht 
 
-capteur = dht.DHT11(Pin(5))
+sensor = dht.DHT22(Pin(5))
+#sensor = dht.DHT11(Pin(22))
 
 while True:
-    try:
-        sleep(1)
-        # Le DHT11 renvoie au maximum une mesure toute les 1s
-        capteur.measure()
-        # Récupère les mesures du capteur
-        print(f"Temperature : {capteur.temperature():.1f}")
-        print(f"Humidite    : {capteur.humidity():.1f}")
-        # Transmet la température sur la console de l'ordinateur
-    except OSError as e:
-        print('Echec reception')
-        # Si la pico ne reçoit pas les mesures du capteur
+  try:
+    sleep(2)
+    sensor.measure()
+    temp = sensor.temperature()
+    hum = sensor.humidity()
+    temp_f = temp * (9/5) + 32.0
+    print('Temperature: %3.1f C' %temp)
+    print('Temperature: %3.1f F' %temp_f)
+    print('Humidity: %3.1f %%' %hum)
+  except OSError as e:
+    print('Failed to read sensor.')
